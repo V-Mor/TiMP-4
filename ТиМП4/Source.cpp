a214b82b5eb4428badcce14db0af99db3df5f042
V-Mor <vvovv266@gmail.com>
2018-04-03 21:38:23 +0700

    general function done

--- ТиМП4/Source.cpp
+++ ТиМП4/Source.cpp
@@ -4,5 +4,14 @@
 int main()
 {
 	Tree<int> der(5);
+	der.addNode(15, 0);
+	der.addNode(15, 1);
+	der.addNode(15, 1);
+	der.addNode(10, 1);
+	der.addNode(15, 3);
+	der.addNode(15, 4);
+	der.addNode(10, 2);
+	der.addNode(15, 5);
+	cout << der.getIncNumber(15, 0);
 	return 0;
 }
--- ТиМП4/Tree.h
+++ ТиМП4/Tree.h
@@ -14,10 +14,13 @@ class Tree
 	node<T> * root;
 	int curr;						//Счётчик текущего идентификатора
 	node<T> * findNode(int, node<T>*);
-public:
-	int addNode(T, int);
+	int getIncNumber(T, node<T>*);
 	void clear(node<T> *);
-	Tree(T x);
+public:
+	void clear(int subtree_id);
+	int getIncNumber(T value, int subtree_id);
+	int addNode(T value, int subtree_id);
+	Tree(T value);
 	~Tree()
 	{
 		clear(root);
@@ -46,10 +49,15 @@ node<T>* Tree<T>::findNode(int id1, node<T> * begin)
 template<class T>
 int Tree<T>::addNode(T x, int id)
 {
+	if (id > curr)
+	{
+		cout << "Нет узла с таким id\n";
+		return NULL;
+	}
 	node<T> * newNode = new node<T>;
 	node<T> * parent = findNode(id, root);
 	if (parent->children == NULL)
-		parent->children = new vector<node*>;
+		parent->children = new vector<node<T>*>;
 	(parent->children)->push_back(newNode);
 	newNode->parent = parent;
 	newNode->id = ++curr;
@@ -57,6 +65,13 @@ int Tree<T>::addNode(T x, int id)
 	newNode->children = NULL;
 	cout << "Создан узел с идентификатором " << curr << endl;
 	return curr;
+}
+
+template<class T>
+void Tree<T>::clear(int id)
+{
+	node<T> * deleting = findNode(id, root);
+	return clear(deleting);
 }
 
 template<class T>
@@ -73,8 +88,34 @@ void Tree<T>::clear(node<T> *deleting)
 		delete deleting;
 	}
 	else
-		for (int i = 0; i <= (deleting->children)->size() - 1; ++i)
+	{
+		int size = (deleting->children)->size() - 1;
+		for (int i = 0; i <= size; ++i)
 			clear((*(deleting->children))[i]);
+	}
+}
+
+template<class T>
+int Tree<T>::getIncNumber(T x, int id)
+{
+	node<T> * checking = findNode(id, root);
+	return getIncNumber(x, checking);
+}
+
+template<class T>
+int Tree<T>::getIncNumber(T x, node<T> * checking)
+{
+	int incNumber = 0;
+	if (x == checking->data)
+		++incNumber;
+	if (!checking->children)
+		return incNumber;
+	else
+	{
+		for (int i = 0; i <= (checking->children)->size() - 1; ++i)
+			incNumber += getIncNumber(x, (*(checking->children))[i]);
+		return incNumber;
+	}
 }
 
 template<class T>
